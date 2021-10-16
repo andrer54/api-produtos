6267cddbb5b17b0ca0c6ae1c031a23b585498f20
andrer54 <andrer54@gmail.com>
2021-10-16 09:10:28 -0300

    routes to post a new produtc, and list one product

--- src/main/java/com/andre/apirest/repository/ProdutoRepository.java
+++ src/main/java/com/andre/apirest/repository/ProdutoRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>
 {
-    
+    Produto findById(long id);
 }
--- src/main/java/com/andre/apirest/resources/ProdutoResource.java
+++ src/main/java/com/andre/apirest/resources/ProdutoResource.java
@@ -7,6 +7,9 @@ import com.andre.apirest.repository.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,16 @@ public class ProdutoResource {
     @GetMapping("/produtos")
     public List<Produto> listaProdutos(){
         return produtoRepository.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto listaProdutoPeloID(@PathVariable(value="id") long id){
+        return produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produto")
+    public Produto salvarProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
     }
 
 }
