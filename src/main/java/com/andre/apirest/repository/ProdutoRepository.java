4fe3eaad020cc617384121c61e892122f6f4f38e
andrer54 <andrer54@gmail.com>
2021-10-16 08:44:28 -0300

    first commit

---
+++ src/main/java/com/andre/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.andre.apirest.repository;
+
+import com.andre.apirest.models.Produto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>
+{
+    
+}
