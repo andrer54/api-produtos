7b06a46b4124f8059b5bf56b0c043446e5b48ae8
andrer54 <andrer54@gmail.com>
2021-10-16 09:20:55 -0300

    controllers to delete and update products from list

--- src/main/java/com/andre/apirest/resources/ProdutoResource.java
+++ src/main/java/com/andre/apirest/resources/ProdutoResource.java
@@ -6,9 +6,11 @@ import com.andre.apirest.models.Produto;
 import com.andre.apirest.repository.ProdutoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +34,16 @@ public class ProdutoResource {
 
     @PostMapping("/produto")
     public Produto salvarProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produto")
+    public void deletarProduto(@RequestBody Produto produto){
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("/produto")
+    public Produto atualizarProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
 
